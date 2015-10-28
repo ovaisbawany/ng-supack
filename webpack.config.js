931fd42f93812ea83138e989b0600cd5a122666a
Umayr Shahid <umayrr@hotmail.co.uk>
2015-10-29 02:27:40 +0500

    Add minimal webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,38 @@
 
 'use strict';
 
+var webpack = require('webpack');
+
 module.exports = {
-  cache: true,
-  entry: './app/src/app.module',
-  output: {
-    filename: 'browser-bundle.js'
-  },
-  module: {
-    loaders: []
-  }
+    context: __dirname + '/app',
+    entry: {
+        app: ['webpack/hot/dev-server', './src/app.module']
+    },
+    output: {
+        path: __dirname + '/build',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                loader: 'style!css!sass'
+            },
+            {
+                test: /\.js$/,
+                loader: 'ng-annotate',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                loader: 'html'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.ProvidePlugin({
+          '_': 'lodash'
+        })
+    ]
 };
