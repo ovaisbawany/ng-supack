bf4d03bad2a7680b17a38978cd432896f81b272d
narsic <narsicnet@gmail.com>
2016-04-24 10:43:02 +0430

    Implement new module "Dashboard" for OcLazyLoad sample

--- app/src/app.module.js
+++ app/src/app.module.js
@@ -7,7 +7,33 @@ angular.module('app', [
     require('oclazyload'),
     require('./common/common.module')
   ])
+  .config(configurationDashboard)
   .config(configurationWelcome);
+
+function configurationDashboard($stateProvider) {
+  $stateProvider
+    .state('Dashboard', {
+      url: '/dashboard',
+      template: require('./dashboard/dashboard.html'),
+      controller: 'Dashboard as vm',
+      title: 'ng-Super Dashboard',
+      resolve: ['$q', '$ocLazyLoad', function ($q, $ocLazyLoad) {
+        var deferred = $q.defer();
+
+        require.ensure([], function() {
+          var mod = require('./dashboard/dashboard.module');
+          $ocLazyLoad.load({
+            name: mod.name
+          });
+
+          deferred.resolve(mod.controller);
+        });
+
+        return deferred.promise;
+      }]
+    });
+
+}
 
 function configurationWelcome($stateProvider) {
   $stateProvider
