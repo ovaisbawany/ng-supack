48ad9da6c02bdd5f18a006ce9754f0a7935b089e
narsic <narsicnet@gmail.com>
2016-04-24 10:40:24 +0430

    Implement OcLazyLoad depend on angular route changing

--- app/src/app.module.js
+++ app/src/app.module.js
@@ -3,7 +3,38 @@
 require('angular');
 
 angular.module('app', [
-  require('./core/core.module'),
-  require('./common/common.module'),
-  require('./welcome/welcome.module')
-]);
+    require('./core/core.module'),
+    require('oclazyload'),
+    require('./common/common.module')
+  ])
+  .config(configurationWelcome);
+
+function configurationWelcome($stateProvider) {
+  $stateProvider
+    .state('Welcome', {
+      url: '/welcome',
+      template: require('./welcome/welcome.html'),
+      controller: 'Welcome as vm',
+      title: 'ng-Super Welcome',
+      resolve: {
+        loadController: function($q, $ocLazyLoad) {
+          return $q(function(resolve)  {
+            require.ensure([], function() {
+              // load only controller module
+              var module = require('./welcome/welcome.module');
+              $ocLazyLoad.load({name: module.name});
+              resolve(module.controller);
+            });
+          });
+        }
+      }
+    });
+
+}
+  function showLoadingState() {
+    console.log('SHOW Loading');
+  }
+
+  function hideLoadingState() {
+    console.log('HIDING Loading');
+  }
